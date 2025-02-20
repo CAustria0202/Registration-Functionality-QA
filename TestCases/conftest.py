d04b702c46b8fd87afb5b2e9b9082ecfd41aad69
CAustria0202 <austriachristian1@gmail.com>
2025-02-20 19:29:28 +0800

    Add Headless testing option in conftest.py

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -2,10 +2,16 @@ import os
 import pytest
 import configparser
 from selenium import webdriver
+#Chrome Browser
 from selenium.webdriver.chrome.service import Service as ServiceChrome
 from webdriver_manager.chrome import ChromeDriverManager
+#Chromium Browser
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.core.os_manager import ChromeType
+#Firefox Browser
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+#Edge Browser
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
@@ -23,20 +29,39 @@ def get_config():
     config.read(config_path)
     browser = config.get("settings", "browser", fallback="chrome")
     url = config.get("settings", "url", fallback="https://google.com/")
+    head = config.getboolean("settings", "head", fallback=False)
 
     print(f"Selected browser: {browser}")
     print(f"Selected URL: {url}")
-    return browser, url
+    print(f"Headless: {head}")
+    return browser, url, head
 
 @pytest.fixture(autouse=True)
 def setup(request):
-        browser, url = get_config()
+        browser, url, head = get_config()
         if browser == "chrome":
             chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument("--disable-notifications")
+            if head:
+                chrome_options.add_argument('--headless')
+                chrome_options.add_argument('--no-sandbox')
+                chrome_options.add_argument('--disable-gpu')
+                chrome_options.add_argument('--window-size=1920,1080')
             driver = webdriver.Chrome(service=ServiceChrome(ChromeDriverManager().install()), options=chrome_options)
+        elif browser == "chromium":
+            chromium_options = webdriver.ChromeOptions()
+            if head:
+                chromium_options.add_argument('--headless')
+                chromium_options.add_argument('--no-sandbox')
+                chromium_options.add_argument('--disable-gpu')
+                chromium_options.add_argument('--window-size=1920,1080')
+            driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chromium_options)
         elif browser == "firefox":
             firefox_options = webdriver.FirefoxOptions()
+            if head:
+                firefox_options.add_argument('--headless')
+                firefox_options.add_argument('--no-sandbox')
+                firefox_options.add_argument('--disable-gpu')
+                firefox_options.add_argument('--window-size=1920,1080')
             driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=firefox_options)
         elif browser == "edge":
             driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
