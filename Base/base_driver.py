cfa7036eea0c69650b4ce6aebb16d57347372899
CAustria0202 <austriachristian1@gmail.com>
2025-02-22 21:12:38 +0800

    Added Error Handling

--- Base/base_driver.py
+++ Base/base_driver.py
@@ -1,3 +1,4 @@
+from selenium.common import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -8,18 +9,30 @@ class BaseDriver:
 
     # Wait for all elements located by the given locator to be present in the DOM
     def wait_for_presence_of_all_elements(self, locator_type, locator):
-        wait = WebDriverWait(self.driver, 30)  
-        list_of_elements = wait.until(EC.presence_of_all_elements_located((locator_type, locator)))
-        return list_of_elements
+        try:
+            wait = WebDriverWait(self.driver, 30)
+            list_of_elements = wait.until(EC.presence_of_all_elements_located((locator_type, locator)))
+            return list_of_elements
+        except TimeoutException:
+            print(f"Error: Element with locator '{locator}' was not found.")
+            return None
 
     # Wait for a single element located by the given locator to be present in the DOM
     def wait_for_presence_of_the_element(self, locator_type, locator):
-        wait = WebDriverWait(self.driver, 30)  
-        element_located = wait.until(EC.presence_of_element_located((locator_type, locator)))
-        return element_located
+        try:
+            wait = WebDriverWait(self.driver, 30)
+            element_located = wait.until(EC.presence_of_element_located((locator_type, locator)))
+            return element_located
+        except TimeoutException:
+            print(f"Error: Element with locator '{locator}' was not found.")
+            return None
 
     # Wait until an element located by the given locator is clickable
     def wait_until_elements_to_be_clickable(self, locator_type, locator):
-        wait = WebDriverWait(self.driver, 30)  
-        clickable_element = wait.until(EC.element_to_be_clickable((locator_type, locator)))
-        return clickable_element
+        try:
+            wait = WebDriverWait(self.driver, 30)
+            clickable_element = wait.until(EC.element_to_be_clickable((locator_type, locator)))
+            return clickable_element
+        except TimeoutException:
+            print(f"Error: Element with locator '{locator}' was not found.")
+            return None
